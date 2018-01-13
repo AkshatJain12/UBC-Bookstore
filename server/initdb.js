924d001d25f297a5392c1c7c67e298ce63828e1d
Akshat Jain <akshatjain119@gmail.com>
2018-01-12 20:47:51 -0800

    Final Commit

---
+++ server/initdb.js
@@ -0,0 +1,119 @@
+db.products.drop()
+db.orders.drop()
+db.users.drop()
+
+db.products.insertMany([
+  {
+    name: "KeyboardCombo",
+    price: 25,
+    quantity: 10,
+    filters: ["Tech", "Stationary"],
+    imageUrl: "https://cpen400a-bookstore.herokuapp.com/images/KeyboardCombo.png"
+  },
+  {
+    name: "Mice",
+    price: 6,
+    quantity: 10,
+    filters: ["Tech", "Stationary"],
+    imageUrl: "https://cpen400a-bookstore.herokuapp.com/images/Mice.png"
+  },
+  {
+    name: "PC1",
+    price: 324,
+    quantity: 5,
+    filters: ["Tech"],
+    imageUrl: "https://cpen400a-bookstore.herokuapp.com/images/PC1.png"
+  },
+  {
+    name: "PC2",
+    price: 376,
+    quantity: 9,
+    filters: ["Tech", "Stationary"],
+    imageUrl: "https://cpen400a-bookstore.herokuapp.com/images/PC2.png"
+  },
+  {
+    name: "PC3",
+    price: 357,
+    quantity: 8,
+    filters: ["Tech", "Stationary"],
+    imageUrl: "https://cpen400a-bookstore.herokuapp.com/images/PC3.png"
+  },
+  {
+    name: "Tent",
+    price: 35,
+    quantity: 0,
+    filters: ["Supplies"],
+    imageUrl: "https://cpen400a-bookstore.herokuapp.com/images/Tent.png"
+  },
+  {
+    name: "Box1",
+    price: 5,
+    quantity: 3,
+    filters: ["Supplies", "Stationary", "Gifts"],
+    imageUrl: "https://cpen400a-bookstore.herokuapp.com/images/Box1.png"
+  },
+  {
+    name: "Box2",
+    price: 7,
+    quantity: 9,
+    filters: ["Supplies", "Stationary", "Gifts"],
+    imageUrl: "https://cpen400a-bookstore.herokuapp.com/images/Box2.png"
+  },
+  {
+    name: "Clothes1",
+    price: 25,
+    quantity: 4,
+    filters: ["Clothing", "Gifts"],
+    imageUrl: "https://cpen400a-bookstore.herokuapp.com/images/Clothes1.png"
+  },
+  {
+    name: "Clothes2",
+    price: 26,
+    quantity: 3,
+    filters: ["Clothing", "Gifts"],
+    imageUrl: "https://cpen400a-bookstore.herokuapp.com/images/Clothes2.png"
+  },
+  {
+    name: "Jeans",
+    price: 35,
+    quantity: 8,
+    filters: ["Clothing", "Gifts"],
+    imageUrl: "https://cpen400a-bookstore.herokuapp.com/images/Jeans.png"
+  },
+  {
+    name: "Keyboard",
+    price: 21,
+    quantity: 1,
+    filters: ["Tech", "Stationary"],
+    imageUrl: "https://cpen400a-bookstore.herokuapp.com/images/Keyboard.png"
+  }
+])
+
+db.orders.insertOne(
+  {
+    cart: "json string for cart object",
+    total: 117
+  }
+)
+
+db.users.insertMany([
+{
+	token : "Xoe2inasd"
+},
+
+{
+	token : "Xoe2inqwe"
+},
+
+{
+	token : "Xoe2infgh"
+},
+
+{
+	token : "Xoe2invbn"
+},
+
+{
+	token : "Xoe2ilkmnui"
+}
+])
